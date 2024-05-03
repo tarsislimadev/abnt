e38720bf418cbfc92fd705f21931ad784836f5c8
Tarsis <br.tmvdl@gmail.com>
2024-05-03 15:38:53 -0300

    Updating app files

---
+++ src/app/public/components/menu.component.js
@@ -0,0 +1,8 @@
+import { HTML, nFlex } from '@brtmvdl/frontend'
+
+export class MenuComponent extends HTML {
+  onCreate() {
+    super.onCreate()
+    this.setText('menu')
+  }
+}
--- src/app/public/index.js
+++ src/app/public/index.js
@@ -1,11 +1,37 @@
-import { PageComponent } from './components/page.component.js'
-import { ContainerComponent } from './components/container.component.js'
+import { HTML, nFlex } from '@brtmvdl/frontend'
+import { MenuComponent } from './components/menu.component.js'
+import { FormComponent } from './components/form.component.js'
+import { PDFComponent } from './components/pdf.component.js'
 
-export class Page extends PageComponent {
-  getBodyComponent() {
-    const html = new ContainerComponent()
-    html.setText('body')
-    html.setText('component')
-    return html
+export class Page extends HTML {
+  children = {
+    menu: new MenuComponent(),
+    form: new FormComponent(),
+    pdf: new PDFComponent(),
+  }
+
+  onCreate() {
+    super.onCreate()
+    this.append(this.getFlex())
+  }
+
+  getFlex() {
+    const flex = new nFlex()
+    flex.append(this.getMenu().setContainerStyle('width', '20%'))
+    flex.append(this.getForm().setContainerStyle('width', '40%'))
+    flex.append(this.getPDF().setContainerStyle('width', '40%'))
+    return flex
+  }
+
+  getMenu() {
+    return this.children.menu
+  }
+
+  getForm() {
+    return this.children.form
+  }
+
+  getPDF() {
+    return this.children.pdf
   }
 }
