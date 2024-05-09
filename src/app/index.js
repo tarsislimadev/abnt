3d5b84d4a273e8be6d7a7422a02dfaf79871e703
Tarsis <br.tmvdl@gmail.com>
2024-05-09 10:34:08 -0300

    Updating app files

--- src/app/index.js
+++ src/app/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const { createServer } = require('http')
 const { Server } = require('socket.io')
-const { PORT } = require('./config.js')
+
 const app = express()
 const server = createServer(app)
 const io = new Server(server)
@@ -10,4 +10,4 @@ app.use(express.static('public'))
 
 io.on('connection', (socket) => console.log('a user connected'))
 
-server.listen(PORT, () => console.log(`Server listening on ${PORT}`))
+server.listen(80, () => console.log('Server listening on 80'))
