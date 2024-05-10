f9d9cc4595c6874669cb547811908c52443117dc
Tarsis <br.tmvdl@gmail.com>
2024-05-10 11:40:10 -0300

    Updating app files

--- src/app/public/components/pdf.component.js
+++ src/app/public/components/pdf.component.js
@@ -3,11 +3,17 @@ import { HTML } from '@brtmvdl/frontend'
 export class PDFComponent extends HTML {
   onCreate() {
     super.onCreate()
+    this.setEvents()
     this.setStyles()
     this.setText('pdf')
+  }
+
+  setEvents() {
+    this.on('update', ({ value: { id, data } }) => console.log('update', { id, data }))
   }
 
   setStyles() {
     this.setStyle('padding', 'calc(1rem / 4)')
   }
+
 }
