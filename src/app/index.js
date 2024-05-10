41e99517726a629514d11f74b422fe63b2aa1d47
Tarsis <br.tmvdl@gmail.com>
2024-05-10 11:33:58 -0300

    Updating app files

--- src/app/index.js
+++ src/app/index.js
@@ -6,7 +6,31 @@ const app = express()
 const server = createServer(app)
 const io = new Server(server)
 
+const { Response, ErrorResponse } = require('./libs/express/index.js')
+
+const { Database } = require('@brtmvdl/database')
+
+const db = new Database({ type: 'fs', config: '/data' })
+
+const documents = db.in('documents')
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
 app.use(express.static('public'))
+
+app.post('/documents/save', ({ body }, res) => {
+  let id = body.id
+  const data = body.data
+  try {
+    let document = documents.find(id)
+    if (!document) document = documents.new()
+    document.writeMany(data)
+    res.json(new Response({ id: document.id, data }))
+  } catch (e) {
+    res.json(new ErrorResponse(e))
+  }
+})
 
 io.on('connection', (socket) => console.log('a user connected'))
 
