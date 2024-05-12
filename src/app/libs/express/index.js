68d29f17d50f0f85496b46e09de08dc35a95b95e
Tarsis <br.tmvdl@gmail.com>
2024-05-12 11:31:12 -0300

    Updating app files

--- src/app/libs/express/index.js
+++ src/app/libs/express/index.js
@@ -1,9 +1,7 @@
 const { Response } = require('./response.js')
 const { ErrorResponse } = require('./error.response.js')
-const { DocumentResponse } = require('./document.response.js')
 
 module.exports = {
   Response,
   ErrorResponse,
-  DocumentResponse,
 }
