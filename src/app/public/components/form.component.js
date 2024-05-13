83cd54c5ae29c4208508f8bfe3197ec829c584cf
Tarsis <br.tmvdl@gmail.com>
2024-05-12 22:00:37 -0300

    Updating app files

--- src/app/public/components/form.component.js
+++ src/app/public/components/form.component.js
@@ -1,10 +1,39 @@
 import { HTML } from '@brtmvdl/frontend'
-
-import * as forms from './forms/index.js'
+import { TextInputGroupComponent } from './text.input.group.component.js'
+import { ButtonComponent } from './button.component.js'
+import * as API from '../utils/api.js'
 
 export class FormComponent extends HTML {
+  state = {
+    id: 0,
+  }
+
   children = {
     form: new HTML(),
+    save: new ButtonComponent(),
+    folhas_rosto: {
+      titulo: new TextInputGroupComponent('titulo', 'Título'),
+      subtitulo: new TextInputGroupComponent('subtitulo', 'Subtítulo'),
+      universidade: new TextInputGroupComponent('universidade', 'Universidade'),
+      instituto: new TextInputGroupComponent('instituto', 'Instituto'),
+      departamento: new TextInputGroupComponent('departamento', 'Departamento'),
+      curso: new TextInputGroupComponent('curso', 'Curso'),
+      local: new TextInputGroupComponent('local', 'Local'),
+      data_defesa: new TextInputGroupComponent('data_defesa', 'Data de Defesa'),
+      volume: new TextInputGroupComponent('volume', 'Volume'),
+      orientador: new TextInputGroupComponent('orientador', 'Orientador'),
+      tipo_trabalho: new TextInputGroupComponent('tipo_trabalho', 'Tipo de Trabalho'),
+      pre_ambulo: new TextInputGroupComponent('pre_ambulo', 'Prêambulo'),
+    },
+    autores: {
+      nome_autor: new TextInputGroupComponent('nome_autor', 'Nome do Autor'),
+      email_autor: new TextInputGroupComponent('email_autor', 'E-mail do Autor'),
+    },
+  }
+
+  constructor(id) {
+    super()
+    this.state.id = id
   }
 
   onCreate() {
@@ -15,30 +44,11 @@ export class FormComponent extends HTML {
   }
 
   setEvents() {
-    this.on('menu', ({ value: { id } }) => this.onMenu(id))
+    this.on('update', (ev) => this.onUpdate(ev))
   }
 
-  onMenu(id) {
-    this.children.form.clear()
-    const form = this.getFormById(id)
-    form.on('save', ({ value }) => this.dispatchEvent('save', value))
-    this.children.form.append(form)
-  }
-
-  getFormById(id) {
-    switch (id) {
-      case 'agradecimentos': return new forms.AgradecimentosForm()
-      case 'anexos': return new forms.AnexosForm()
-      case 'apendices': return new forms.ApendicesForm()
-      case 'autores': return new forms.AutoresForm()
-      case 'dedicatoria': return new forms.DedicatoriaForm()
-      case 'folha-rosto': return new forms.FolhaRostoForm()
-      case 'glossario': return new forms.GlossarioForm()
-      case 'resumo': return new forms.ResumoForm()
-      case 'texto-principal': return new forms.TextoPrincipalForm()
-    }
-
-    return new HTML()
+  onUpdate({ value } = {}) {
+    console.log('on update', { value })
   }
 
   setStyles() {
@@ -46,6 +56,52 @@ export class FormComponent extends HTML {
   }
 
   getForm() {
+    this.children.form.append(this.children.folhas_rosto.titulo)
+    this.children.form.append(this.children.folhas_rosto.subtitulo)
+    this.children.form.append(this.children.folhas_rosto.universidade)
+    this.children.form.append(this.children.folhas_rosto.instituto)
+    this.children.form.append(this.children.folhas_rosto.departamento)
+    this.children.form.append(this.children.folhas_rosto.curso)
+    this.children.form.append(this.children.folhas_rosto.local)
+    this.children.form.append(this.children.folhas_rosto.data_defesa)
+    this.children.form.append(this.children.folhas_rosto.volume)
+    this.children.form.append(this.children.folhas_rosto.orientador)
+    this.children.form.append(this.children.folhas_rosto.tipo_trabalho)
+    this.children.form.append(this.children.folhas_rosto.pre_ambulo)
+    this.children.form.append(this.children.autores.nome_autor)
+    this.children.form.append(this.children.autores.email_autor)
+    this.children.form.append(this.getSaveButton())
     return this.children.form
+  }
+
+  getSaveButton() {
+    this.children.save.setText('save')
+    this.children.save.on('click', () => this.onSaveButtonClick())
+    return this.children.save
+  }
+
+  onSaveButtonClick() {
+    API.saveDocument(this.getData(), this.state.id)
+      .then(() => this.dispatchEvent('save', this.getData()))
+      .catch((err) => console.error(err))
+  }
+
+  getData() {
+    return {
+      titulo: this.children.folhas_rosto.titulo.children.input.getValue(),
+      subtitulo: this.children.folhas_rosto.subtitulo.children.input.getValue(),
+      universidade: this.children.folhas_rosto.universidade.children.input.getValue(),
+      instituto: this.children.folhas_rosto.instituto.children.input.getValue(),
+      departamento: this.children.folhas_rosto.departamento.children.input.getValue(),
+      curso: this.children.folhas_rosto.curso.children.input.getValue(),
+      local: this.children.folhas_rosto.local.children.input.getValue(),
+      data_defesa: this.children.folhas_rosto.data_defesa.children.input.getValue(),
+      volume: this.children.folhas_rosto.volume.children.input.getValue(),
+      orientador: this.children.folhas_rosto.orientador.children.input.getValue(),
+      tipo_trabalho: this.children.folhas_rosto.tipo_trabalho.children.input.getValue(),
+      pre_ambulo: this.children.folhas_rosto.pre_ambulo.children.input.getValue(),
+      nome_autor: this.children.autores.nome_autor.children.input.getValue(),
+      email_autor: this.children.autores.email_autor.children.input.getValue(),
+    }
   }
 }
--- src/app/public/index.js
+++ src/app/public/index.js
@@ -1,5 +1,4 @@
 import { HTML, nFlex } from '@brtmvdl/frontend'
-import { MenuComponent } from './components/menu.component.js'
 import { FormComponent } from './components/form.component.js'
 import { PDFComponent } from './components/pdf.component.js'
 import { StateModel } from './models/state.model.js'
@@ -7,7 +6,6 @@ import * as API from './utils/api.js'
 
 export class Page extends HTML {
   children = {
-    menu: new MenuComponent(),
     form: new FormComponent(),
     pdf: new PDFComponent(),
   }
@@ -30,20 +28,9 @@ export class Page extends HTML {
 
   getFlex() {
     const flex = new nFlex()
-    flex.append(this.getMenu())
     flex.append(this.getForm())
     flex.append(this.getPDF())
     return flex
-  }
-
-  getMenu() {
-    this.children.menu.setContainerStyle('width', '20%')
-    this.children.menu.on('menu', ({ value: { id } }) => this.onMenuChange(id))
-    return this.children.menu
-  }
-
-  onMenuChange(id) {
-    this.children.form.dispatchEvent('menu', { id })
   }
 
   getForm() {
@@ -58,6 +45,7 @@ export class Page extends HTML {
     API.saveDocument(this.state.get('data'), this.state.get('id'))
       .then((res) => res.getData())
       .then(({ id }) => this.state.update('id', id))
+      .then(() => this.children.form.dispatchEvent('update', this.state.toJSON()))
       .then(() => this.children.pdf.dispatchEvent('update', this.state.toJSON()))
       .catch((err) => console.error(err))
   }
