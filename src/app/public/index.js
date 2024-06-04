2a2501bea9fea8f45de2285184de23328b9b0913
Tarsis <br.tmvdl@gmail.com>
2024-06-04 10:57:56 -0300

    Updating app files

--- src/app/public/index.js
+++ src/app/public/index.js
@@ -2,7 +2,6 @@ import { HTML, nFlex } from '@brtmvdl/frontend'
 import { FormComponent } from './components/form.component.js'
 import { PDFComponent } from './components/pdf.component.js'
 import { StateModel } from './models/state.model.js'
-import * as API from './utils/api.js'
 
 export class Page extends HTML {
   children = {
@@ -37,17 +36,6 @@ export class Page extends HTML {
     this.children.form.setContainerStyle('width', '40%')
     this.children.form.on('save', ({ value }) => this.saveDocument(value))
     return this.children.form
-  }
-
-  saveDocument(data = {}) {
-    this.state.update('data', data)
-
-    API.saveDocument(this.state.get('data'), this.state.get('id'))
-      .then((res) => res.getData())
-      .then(({ id }) => this.state.update('id', id))
-      .then(() => this.children.form.dispatchEvent('update', this.state.toJSON()))
-      .then(() => this.children.pdf.dispatchEvent('update', this.state.toJSON()))
-      .catch((err) => console.error(err))
   }
 
   getPDF() {
